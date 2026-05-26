f9a2ab7a4d4a2e35452ddeba68f1afa3dfd6ffca
savio-alexandre <alexandresaviosouza@gmail.com>
2026-05-26 16:28:14 -0300

    operadores de atribuição simples e combinada em js

---
+++ fullstack-2026/modulo-01-logica-js/fundamentos/aula017/operadoresAtribuicao.js
@@ -0,0 +1,23 @@
+//Operador de Atribuição Simples (=): Atribui o valor à variável.
+let x = 10;  // Atribui 10 à variável x
+console.log(x);
+
+
+/**
+ * Operadores de Atribuição Combinada: Estes operadores realizam
+ * uma operação aritmética e atribuem o resultado à variável.
+ */
+let a = 5;
+a += 3;  // Equivalente a: a = a + 3; (adiciona 3 a 'a')
+
+let b = 8;
+b -= 2;  // Equivalente a: b = b - 2; (subtrai 2 de 'b')
+
+let c = 4;
+c *= 6;  // Equivalente a: c = c * 6; (multiplica 'c' por 6)
+
+let d = 9;
+d /= 3;  // Equivalente a: d = d / 3; (divide 'd' por 3)
+
+let e = 10;
+e %= 3;  // Equivalente a: e = e % 3; (atribui o resto da divisão de 'e' por 3 a 'e')
