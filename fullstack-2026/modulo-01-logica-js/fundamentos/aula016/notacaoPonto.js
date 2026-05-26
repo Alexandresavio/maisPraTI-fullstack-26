3b76075ff9778bbfd61f64f71f035d837dc58941
savio-alexandre <alexandresaviosouza@gmail.com>
2026-05-26 16:17:31 -0300

    notação ponto

---
+++ fullstack-2026/modulo-01-logica-js/fundamentos/aula016/notacaoPonto.js
@@ -0,0 +1,30 @@
+/**
+ * O que é notação ponto?
+ * A notação ponto é usada para:
+ * acessar propriedades,
+ * acessar métodos,
+ * criar atributos dinamicamente,
+ * acessar membros de objetos e funções.
+ */
+
+/**
+ * Exemplo:
+ * console é um objeto,
+ * log é uma função acessada usando notação ponto(.)
+ */
+console.log();
+
+/**
+ * Nesse exemplo:
+ * Math é um objeto, 
+ * ceil é um método acessado pela notação ponto.
+ */
+console.log(Math.ceil(6.1));
+
+/**
+ * Criando atributos dinamicamente:
+ * é possível criar propriedades em objetos usando notação ponto.
+ */
+const obj = {};
+obj.nome = 'Bola';
+console.log(obj);
