d1fc5fd808d771d997e409dddc7332dbce9e16f0
savio-alexandre <alexandresaviosouza@gmail.com>
2026-05-27 16:48:24 -0300

    adiciona arquivo sobre operadores relacionais  JS

---
+++ fullstack-2026/modulo-01-logica-js/fundamentos/aula017/operadorRelacional.js
@@ -0,0 +1,44 @@
+/**
+ * Em Javascript, os operadores relacionais são utilizados para comparar 
+ * valores e retornar um valor booleano (verdadeiro ou falso) com base na
+ * relação entre esses valores. 
+ */
+
+/**
+ * Igual (==): Verifica se dois valores são iguais, 
+ * mas não leva em consideração o tipo.
+ */
+console.log(5 == "5"); // true
+
+/**
+ * Estritamente igual (===): Verifica se dois valores são iguais
+ * e do mesmo tipo.
+ */
+console.log(5 === "5"); // false
+
+/**
+ * Diferente (!=): Verifica se dois valores não são iguais,
+ * mas não leva em consideração o tipo. 
+ * Realiza a coerção de tipo, se necessário.
+ */
+console.log(5 != "5"); // false
+
+/**
+ * Maior que (>): Verifica se o valor da esquerda é estritamente maior que o valor da direita.
+ */
+console.log(10 > 5); // true
+
+/**
+ * Menor que (<): Verifica se o valor da esquerda é estritamente menor que o valor da direita.
+ */
+console.log(3 < 8); // true
+
+/**
+ * Maior ou igual a (>=): Verifica se o valor da esquerda é maior ou igual ao valor da direita.
+ */
+console.log(7 >= 7); // true
+
+/**
+ * Menor ou igual a (<=): Verifica se o valor da esquerda é menor ou igual ao valor da direita.
+ */
+console.log(4 <= 6); // true
