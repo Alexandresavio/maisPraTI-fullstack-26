f81781e0b5fa3f5a01a19edc18386c1f9b770fa7
savio-alexandre <alexandresaviosouza@gmail.com>
2026-06-11 22:42:52 -0300

    adiciona arquivos de funcoes em js e html basico

---
+++ fullstack-2026/modulo-01-logica-js/funcoes-JS/aula01/cidadaoPrimeiraClasse.js
@@ -0,0 +1,37 @@
+/**
+ * O que é uma função?
+ * Uma função é um bloco de código reutilizável que pode:
+ * Receber parâmetros;
+ * Executar uma determinada lógica;
+ * Retornar um valor.
+ */
+
+/**
+ * Declarando funções:
+ * A forma mais tradicional é a declaração literal:
+ */
+function soma(a, b){
+    return a + b;
+}
+/**
+ * Os parâmetros são opcionais;
+ * O retorno é opcional;
+ * Se nenhum valor for retornado, a função retorna undefined;
+ * Diferentemente de algumas estruturas de controle, o bloco {} * é obrigatório em funções tradicionais.
+ */
+
+/**
+ * Armazenando funções em variáveis
+ * Funções podem ser atribuídas a constantes ou variáveis:
+ */
+/**
+ * Nesse exemplo:
+ * A função é anônima (não possui nome);
+ * A variável passa a referenciar a função;
+ * Para executá-la, é necessário utilizar parênteses:
+ */
+const funcao = function(){
+    console.log("Olá!");
+}
+
+funcao();
